a6c024cd0ea20fe2eec3d072aba4262e5f20ff8f
Graham Heathcote <grahamkheathcote@gmail.com>
2026-05-22 23:34:37 +0100

    works

--- main.jl
+++ main.jl
@@ -2,53 +2,66 @@ using Gym
 using Flux
 using Distributions
 
+env = Gym.make("CartPole-v1"; render_mode=:human)
+action_rng = Gym.seed(123)
+init_state, info = Gym.reset!(env)
+
 State = Array{Union{AbstractMatrix{Float32}, Bool}, 4}
 
-struct Episode where
-    curr::State,
-    action::Bool,
-    reward::Float32,
-    next::State
-end
+# struct Episode where
+#     curr::State,
+#     action::Bool,
+#     reward::Float32,
+#     next::State
+# end
 
 function select_action(
         epsilon,
-        model
+        model,
+        state
     )
-
     if rand() < epsilon
-        # choose random action
+        return rand(action_rng, Gym.action_space(env))
     end
-    # choose optimal action
+    Int(round(only(model(only(state)))))
 end
 
-model = Chain(
+function init_network()
+    model = Chain(
     Dense(4 => 4, relu),
     Dense(4 => 4, relu),
     Dense(4 => 1, sigmoid))
 
-optimizer = Flux.setup(Flux.Adam(0.01), model)
+    optimizer = Flux.setup(Flux.Adam(0.01), model)
+
+    model, optimizer
+end
+
 
 function train(
         capacity::Int,
         episodes::Int64,
         steps::Int64,
         epsilon::Float32,
-        model
     )
 
-    init_memory(capacity)
-    init_network()
+    # init_memory(capacity)
+    model, optimizer = init_network()
 
     sample_size = 100
 
     for episode in 1:episodes 
-        state = [get_screen()]
-        phi = process(state)
+        state = [init_state]
+        # phi = process(state)
+        Gym.reset!(env)
         for t in 1:steps 
 
-            action = select_action(epsilon)
+            action = select_action(epsilon, model, state)
+            observation, reward, terminated, truncated, info = Gym.step!(env, action)
 
+            if terminated || truncated
+                break
+            end
             # exec action
 
             # update state / phi
@@ -59,21 +72,17 @@ function train(
 
             # y_j = ...
 
-            gradient = calc_gradient()
-            update_wi
+            # gradient = calc_gradient()
+            # update_wi
         end
+
+        
 
     end
 
 end
 
 
-env = Gym.make("CartPole-v1"; render_mode=:human)
-observation, info = Gym.reset!(env)
-
-while true
-    action = rand(Gym.action_space(env))
-    observation, reward, terminated, truncated, info = Gym.step!(env, action)
-end
+train(1, 100, 100, 0.1f0)
 
 Gym.close(env)
