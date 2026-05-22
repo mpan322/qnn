501348e404c93e3cfe40dbdd02773ef525b003c0
Graham Heathcote <grahamkheathcote@gmail.com>
2026-05-22 21:49:51 +0100

    works

--- main.jl
+++ main.jl
@@ -1,2 +1,14 @@
-using 
+using Gym
 
+env = Gym.make("CartPole-v1"; render_mode=:human)
+observation, info = Gym.reset!(env)
+
+while true
+    action = rand(Gym.action_space(env))
+    observation, reward, terminated, truncated, info = Gym.step!(env, action)
+    if terminated || truncated
+        break
+    end
+end
+
+Gym.close(env)
